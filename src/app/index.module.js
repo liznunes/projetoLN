caa22d6b998d0e60ccc30da8a0efbf83f9ebc38e
liznunes <liz_nunes@yahoo.com.br>
2017-12-26 21:35:24 -0400

    commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('projetoLn', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
